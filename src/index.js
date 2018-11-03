083279e641200a3cb6dd35270f0860550d12b634
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-03 09:43:04 -0500

    setup remastered

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import $ from 'jquery';
 import 'bootstrap';
-
 import './index.scss';
 
-$('#click-button').on('click', () => {
-  $('#important-text').append('<p>I like dogs</p>');
-});
+$('#dogs').show();
